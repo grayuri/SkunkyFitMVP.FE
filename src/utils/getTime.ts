147a3ae4fe84acec63c45625fdb9d2bb098a3e72
grayuri <bv6778145@gmail.com>
2025-01-20 15:40:42 -0300

    Feat: Meals Dates Issues Fixed

--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -1,6 +1,6 @@
 export default function getTime(time: Date): string {
   const date = new Date(time)
-  const hours = date.getHours()
+  const hours = date.getHours() + 2
   const minutes = date.getMinutes()
 
   return `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`
